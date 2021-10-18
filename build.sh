da2f45db9129fbc59282e305f8563853b8c4c9cd
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2021-10-18 22:17:57 +0300

    Finish TODOs

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 func -SPA stdlib.fc wallet-v4-code.fc -o wallet-v4-code.fif
 func -SPA stdlib.fc subscription-plugin.fc -o subscription-plugin-code.fif
+
+sed -i '1 ax{FA04} dup @Defop LDEXTRA @Defop LDVARUINT32' wallet-v4-code.fif
+
 fift wallet-v4-code.fif
 fift subscription-plugin-code.fif
