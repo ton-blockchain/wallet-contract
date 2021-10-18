04a7b09164c4c583da74eeba88d3e0c795f37002
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2021-10-18 21:12:36 +0300

    Add Readme

---
+++ build.sh
@@ -0,0 +1,4 @@
+func -SPA stdlib.fc wallet-v4-code.fc -o wallet-v4-code.fif
+func -SPA stdlib.fc subscription-plugin.fc -o subscription-plugin-code.fif
+fift wallet-v4-code.fif
+fift subscription-plugin-code.fif
