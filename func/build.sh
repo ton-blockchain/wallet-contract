1fb81d42db5e54add9f8943d3dcb1a459462912f
tolya-yanot <tolyayanot@gmail.com>
2021-12-16 21:15:30 +0300

    gas_to_coins

--- func/build.sh
+++ func/build.sh
@@ -1,4 +1,4 @@
 func -o wallet-v4-code.fif -SPA stdlib.fc wallet-v4-code.fc
 func -o subscription-plugin-code.fif -SPA stdlib.fc simple-subscription-plugin.fc
 
-fift print-hext.fif
+fift print-hex.fif
